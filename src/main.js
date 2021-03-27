f84ad9c4fe2b1e3fb2c6ae0d33f565a4a6f6a84b
Efrain Hernandez <efrainhg2050@gmail.com>
2021-03-27 17:24:17 -0600

    Fetch to Avocado API

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("Hi insie the console");
+const urlAPI = "https://platzi-avo.vercel.app/api/avo";
+let avocadoData = [];
+
+fetch(urlAPI)
+    .then(response => response.json())
+    .then(datos => avocadoData = datos.data)
+    .catch(error => console.error(error + " Data API"));
