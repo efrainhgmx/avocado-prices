1b66ddc929e0bf8e97fb0479b6e6fbaf65839b75
Efrain Hernandez <efrainhg2050@gmail.com>
2021-03-27 22:47:45 -0600

    API fetch using promises

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 const urlAPI = "https://platzi-avo.vercel.app/api/avo";
-let avocadoData = [];
+const avocadoGallery = document.getElementById('avocadoGallery');
 
-const showData = () => {
-    const avocadoContainer = document.createElement('div');
-    document.querySelector('.main-content').appendChild("Hola", avocadoContainer);
-}
 fetch(urlAPI)
     .then(response => response.json())
-    .then(datos => avocadoData = datos.data)
+    .then(datos => {
+        const avocados = [];
+
+        datos.data.forEach(element => {
+            const card = document.createElement('div');
+            const imageContainer = document.createElement('figure');
+
+            const image = document.createElement('img');
+            image.src = `https://platzi-avo.vercel.app${element.image}`;
+
+            const title = document.createElement('h3');
+            title.textContent = element.name;
+
+            const description = document.createElement('p');
+            description.textContent = element.attributes.description;
+
+            const price = document.createElement('span');
+            price.textContent = element.price;
+
+            const serialNumber = document.createElement('p');
+            serialNumber.textContent = element.sku;
+
+            imageContainer.appendChild(image);
+            card.append(imageContainer, title, description, price, serialNumber);
+            avocados.push(card);
+        });
+        avocadoGallery.append(...avocados);
+    })
     .catch(error => console.error(error + " Data API"));
