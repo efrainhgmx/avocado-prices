0c5601d7f97cd0417e8469ca8f39b148ade83151
Efrain Hernandez <efrainhg2050@gmail.com>
2021-03-27 22:53:15 -0600

    new function and better readability

--- src/main.js
+++ src/main.js
@@ -1,34 +1,36 @@
 const urlAPI = "https://platzi-avo.vercel.app/api/avo";
 const avocadoGallery = document.getElementById('avocadoGallery');
 
+function showData(apiData) {
+    const avocados = [];
+
+    apiData.data.forEach(element => {
+        const card = document.createElement('div');
+        const imageContainer = document.createElement('figure');
+
+        const image = document.createElement('img');
+        image.src = `https://platzi-avo.vercel.app${element.image}`;
+
+        const title = document.createElement('h3');
+        title.textContent = element.name;
+
+        const description = document.createElement('p');
+        description.textContent = element.attributes.description;
+
+        const price = document.createElement('span');
+        price.textContent = element.price;
+
+        const serialNumber = document.createElement('p');
+        serialNumber.textContent = element.sku;
+
+        imageContainer.appendChild(image);
+        card.append(imageContainer, title, description, price, serialNumber);
+        avocados.push(card);
+    });
+    avocadoGallery.append(...avocados);
+}
+
 fetch(urlAPI)
     .then(response => response.json())
-    .then(datos => {
-        const avocados = [];
-
-        datos.data.forEach(element => {
-            const card = document.createElement('div');
-            const imageContainer = document.createElement('figure');
-
-            const image = document.createElement('img');
-            image.src = `https://platzi-avo.vercel.app${element.image}`;
-
-            const title = document.createElement('h3');
-            title.textContent = element.name;
-
-            const description = document.createElement('p');
-            description.textContent = element.attributes.description;
-
-            const price = document.createElement('span');
-            price.textContent = element.price;
-
-            const serialNumber = document.createElement('p');
-            serialNumber.textContent = element.sku;
-
-            imageContainer.appendChild(image);
-            card.append(imageContainer, title, description, price, serialNumber);
-            avocados.push(card);
-        });
-        avocadoGallery.append(...avocados);
-    })
+    .then(datos => showData(datos))
     .catch(error => console.error(error + " Data API"));
